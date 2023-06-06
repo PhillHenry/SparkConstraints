2f14209b6da8676bb2e9efccfead8f1c1c79ac90
PhillipHenry <PhillHenry@gmail.com>
2023-06-06 14:14:17 +0100

    Now we can hijack the machinary.

--- src/main/scala/uk/co/odinconsultants/di/ReplaceHiveWriter.scala
+++ src/main/scala/uk/co/odinconsultants/di/ReplaceHiveWriter.scala
@@ -2,20 +2,18 @@ package uk.co.odinconsultants.di
 import org.apache.spark.sql.catalyst.expressions.PredicateHelper
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.rules.Rule
-import org.apache.spark.sql.hive.execution.InsertIntoHiveTable
+import org.apache.spark.sql.hive.execution.CreateHiveTableAsSelectCommand
 
 class ReplaceHiveWriter extends Rule[LogicalPlan] with PredicateHelper {
 
   override def apply(plan: LogicalPlan): LogicalPlan = plan.transformDown {
-    case x @ InsertIntoHiveTable(
-          table,
-          partition,
+    case x @ CreateHiveTableAsSelectCommand(
+          tableDesc,
           query,
-          overwrite,
-          ifPartitionNotExists,
           outputColumnNames,
+          mode
         ) =>
-      println(x)
+      println(s"x = $x")
       x
   }
 
