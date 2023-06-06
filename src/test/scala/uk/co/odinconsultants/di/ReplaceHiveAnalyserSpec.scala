250d42d8fa5c614485b9b2d319b6ffd8c5049163
PhillipHenry <PhillHenry@gmail.com>
2023-06-06 14:32:54 +0100

    Working test. Documentation is misleading though.

--- src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
+++ src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
@@ -1,13 +1,20 @@
 package uk.co.odinconsultants.di
+import org.apache.spark.sql.DataFrame
 import uk.co.odinconsultants.SparkForTesting._
 import org.scalatest.wordspec.AnyWordSpec
 
 class ReplaceHiveAnalyserSpec extends AnyWordSpec {
   "Spark" when {
     "given our configuration" should {
-      val df = spark.createDataFrame(Seq((1, "a"), (2, "b")))
-      df.show()
-      df.writeTo("spark_file_test_writeTo").create()
+      "read and write to metastore" in {
+        val data: Seq[(Int, String)] = Seq((1, "a"), (2, "b"))
+        val df: DataFrame            = spark.createDataFrame(data)
+        val tableName                = "spark_file_test_writeTo"
+        df.show()
+        df.writeTo(tableName).create()
+        val output: DataFrame        = spark.read.table(tableName)
+        assert(output.collect().length == data.length)
+      }
     }
   }
 }
