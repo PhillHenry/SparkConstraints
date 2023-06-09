88e40cce2164b833c8f54b35b84f27a823816e47
PhillipHenry <PhillHenry@gmail.com>
2023-06-09 12:41:10 +0100

    Add assertion.

--- src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
+++ src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
@@ -1,32 +1,27 @@
 package uk.co.odinconsultants.di
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{IntegerType, Metadata, MetadataBuilder, StringType, StructField, StructType}
-import uk.co.odinconsultants.SparkForTesting._
-import org.scalatest.wordspec.AnyWordSpec
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{Metadata, MetadataBuilder}
+import org.scalatest.wordspec.AnyWordSpec
+import uk.co.odinconsultants.SparkForTesting._
 
 class ReplaceHiveAnalyserSpec extends AnyWordSpec {
   "Spark" when {
     "given our configuration" should {
       "read and write to metastore" in {
         val data: Seq[(Int, String)] = Seq((1, "a"), (2, "b"))
-        val builder = new MetadataBuilder
+        val builder                  = new MetadataBuilder
         builder.putLong("max", 2)
-
-        val intField: StructField = StructField("an_int", IntegerType, false, builder.build())
-        val struct: StructType  = StructType(
-          Seq(
-            intField,
-            StructField("a_string", StringType, false, Metadata.empty),
-          )
-        )
-        val colName            = "_1"
-        val df    : DataFrame   = spark.createDataFrame(data).withColumn(colName, col(colName).as(colName, builder.build()))
-        val tableName           = "spark_file_test_writeTo"
+        val intMetadata: Metadata    = builder.build()
+        val colName                  = "_1"
+        val df: DataFrame            =
+          spark.createDataFrame(data).withColumn(colName, col(colName).as(colName, intMetadata))
+        val tableName                = "spark_file_test_writeTo"
         df.show()
         df.writeTo(tableName).create()
         val output: DataFrame        = spark.read.table(tableName)
         assert(output.collect().length == data.length)
+        assert(output.schema.fields.filter(_.name == colName).head.metadata == intMetadata)
       }
     }
   }
