e61be37218f5dad4e6b501a45852d5a738bcdc23
PhillipHenry <PhillHenry@gmail.com>
2023-06-09 12:46:24 +0100

    Refactor,

--- src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
+++ src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
@@ -6,22 +6,26 @@ import org.scalatest.wordspec.AnyWordSpec
 import uk.co.odinconsultants.SparkForTesting._
 
 class ReplaceHiveAnalyserSpec extends AnyWordSpec {
+  val IntField: String = "_1"
   "Spark" when {
     "given our configuration" should {
+
+      val data: Seq[(Int, String)] = Seq((1, "a"), (2, "b"))
+      val builder                  = new MetadataBuilder
+      builder.putLong("max", 2)
+      val intMetadata: Metadata    = builder.build()
+      val df: DataFrame            =
+        spark.createDataFrame(data).withColumn(IntField, col(IntField).as(IntField, intMetadata))
+
       "read and write to metastore" in {
-        val data: Seq[(Int, String)] = Seq((1, "a"), (2, "b"))
-        val builder                  = new MetadataBuilder
-        builder.putLong("max", 2)
-        val intMetadata: Metadata    = builder.build()
-        val colName                  = "_1"
-        val df: DataFrame            =
-          spark.createDataFrame(data).withColumn(colName, col(colName).as(colName, intMetadata))
-        val tableName                = "spark_file_test_writeTo"
+        val tableName         = "spark_file_test_writeTo"
         df.show()
         df.writeTo(tableName).create()
-        val output: DataFrame        = spark.read.table(tableName)
+        val output: DataFrame = spark.read.table(tableName)
         assert(output.collect().length == data.length)
-        assert(output.schema.fields.filter(_.name == colName).head.metadata == intMetadata)
+        val outputMetadata: Metadata = output.schema.fields.filter(_.name == IntField).head.metadata
+        assert(outputMetadata == intMetadata)
+        println(outputMetadata.json)
       }
     }
   }
