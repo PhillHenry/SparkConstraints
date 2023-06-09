c9303911d01f3227d9877415d840db11d3bbc092
PhillipHenry <PhillHenry@gmail.com>
2023-06-09 12:37:48 +0100

    Add metadata

--- src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
+++ src/test/scala/uk/co/odinconsultants/di/ReplaceHiveAnalyserSpec.scala
@@ -1,15 +1,28 @@
 package uk.co.odinconsultants.di
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types.{IntegerType, Metadata, MetadataBuilder, StringType, StructField, StructType}
 import uk.co.odinconsultants.SparkForTesting._
 import org.scalatest.wordspec.AnyWordSpec
+import org.apache.spark.sql.functions._
 
 class ReplaceHiveAnalyserSpec extends AnyWordSpec {
   "Spark" when {
     "given our configuration" should {
       "read and write to metastore" in {
         val data: Seq[(Int, String)] = Seq((1, "a"), (2, "b"))
-        val df: DataFrame            = spark.createDataFrame(data)
-        val tableName                = "spark_file_test_writeTo"
+        val builder = new MetadataBuilder
+        builder.putLong("max", 2)
+
+        val intField: StructField = StructField("an_int", IntegerType, false, builder.build())
+        val struct: StructType  = StructType(
+          Seq(
+            intField,
+            StructField("a_string", StringType, false, Metadata.empty),
+          )
+        )
+        val colName            = "_1"
+        val df    : DataFrame   = spark.createDataFrame(data).withColumn(colName, col(colName).as(colName, builder.build()))
+        val tableName           = "spark_file_test_writeTo"
         df.show()
         df.writeTo(tableName).create()
         val output: DataFrame        = spark.read.table(tableName)
