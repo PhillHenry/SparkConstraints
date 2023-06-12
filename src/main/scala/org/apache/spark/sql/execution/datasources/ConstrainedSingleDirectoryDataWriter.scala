9df86b70c502d6d6f3153517df5a1096b6e8e543
PhillipHenry <PhillHenry@gmail.com>
2023-06-12 11:00:08 +0100

    Oops. Now the writer is subverted.

--- src/main/scala/org/apache/spark/sql/execution/datasources/ConstrainedSingleDirectoryDataWriter.scala
+++ src/main/scala/org/apache/spark/sql/execution/datasources/ConstrainedSingleDirectoryDataWriter.scala
@@ -1,6 +1,7 @@
 package org.apache.spark.sql.execution.datasources
 import org.apache.hadoop.mapreduce.TaskAttemptContext
 import org.apache.spark.internal.io.FileCommitProtocol
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.execution.metric.SQLMetric
 
 class ConstrainedSingleDirectoryDataWriter(
@@ -8,4 +9,10 @@ class ConstrainedSingleDirectoryDataWriter(
     taskAttemptContext: TaskAttemptContext,
     committer: FileCommitProtocol,
     customMetrics: Map[String, SQLMetric] = Map.empty,
-) extends SingleDirectoryDataWriter(description, taskAttemptContext, committer, customMetrics) {}
+) extends SingleDirectoryDataWriter(description, taskAttemptContext, committer, customMetrics) {
+  println("ConstrainedSingleDirectoryDataWriter")
+  override def write(record: InternalRow): Unit = {
+    println(s"record = $record, meta = ${description.allColumns}")
+    super.write(record)
+  }
+}
--- src/main/scala/org/apache/spark/sql/hive/execution/DecoratedInsertIntoHiveTable.scala
+++ src/main/scala/org/apache/spark/sql/hive/execution/DecoratedInsertIntoHiveTable.scala
@@ -1,6 +1,15 @@
 package org.apache.spark.sql.hive.execution
-import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.internal.io.FileCommitProtocol
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.catalog.CatalogTypes.TablePartitionSpec
+import org.apache.spark.sql.catalyst.catalog.{BucketSpec, CatalogTable}
+import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.execution.datasources.{BucketingUtils, ConstrainedFileFormatWriter}
+
+import java.util.Locale
 
 class DecoratedInsertIntoHiveTable(
                                     table: CatalogTable,
@@ -15,5 +24,70 @@ class DecoratedInsertIntoHiveTable(
   overwrite: Boolean,
   ifPartitionNotExists: Boolean,
   outputColumnNames: Seq[String]) {
+
+  override protected def saveAsHiveFile(
+      sparkSession: SparkSession,
+      plan: SparkPlan,
+      hadoopConf: Configuration,
+      fileSinkConf: org.apache.spark.sql.hive.HiveShim.ShimFileSinkDesc,
+      outputLocation: String,
+      customPartitionLocations: Map[TablePartitionSpec, String] = Map.empty,
+      partitionAttributes: Seq[Attribute] = Nil,
+      bucketSpec: Option[BucketSpec] = None,
+  ): Set[String] = {
+
+    val isCompressed =
+      fileSinkConf.getTableInfo.getOutputFileFormatClassName.toLowerCase(Locale.ROOT) match {
+        case formatName if formatName.endsWith("orcoutputformat") =>
+          // For ORC,"mapreduce.output.fileoutputformat.compress",
+          // "mapreduce.output.fileoutputformat.compress.codec", and
+          // "mapreduce.output.fileoutputformat.compress.type"
+          // have no impact because it uses table properties to store compression information.
+          false
+        case _                                                    => hadoopConf.get("hive.exec.compress.output", "false").toBoolean
+      }
+
+    if (isCompressed) {
+      hadoopConf.set("mapreduce.output.fileoutputformat.compress", "true")
+      fileSinkConf.setCompressed(true)
+      fileSinkConf.setCompressCodec(
+        hadoopConf
+          .get("mapreduce.output.fileoutputformat.compress.codec")
+      )
+      fileSinkConf.setCompressType(
+        hadoopConf
+          .get("mapreduce.output.fileoutputformat.compress.type")
+      )
+    } else {
+      // Set compression by priority
+      HiveOptions
+        .getHiveWriteCompression(fileSinkConf.getTableInfo, sparkSession.sessionState.conf)
+        .foreach { case (compression, codec) => hadoopConf.set(compression, codec) }
+    }
+
+    val committer = FileCommitProtocol.instantiate(
+      sparkSession.sessionState.conf.fileCommitProtocolClass,
+      jobId = java.util.UUID.randomUUID().toString,
+      outputPath = outputLocation,
+    )
+
+    val options = bucketSpec
+      .map(_ => Map(BucketingUtils.optionForHiveCompatibleBucketWrite -> "true"))
+      .getOrElse(Map.empty)
+
+    ConstrainedFileFormatWriter.write(
+      sparkSession = sparkSession,
+      plan = plan,
+      fileFormat = new HiveFileFormat(fileSinkConf),
+      committer = committer,
+      outputSpec = ConstrainedFileFormatWriter
+        .OutputSpec(outputLocation, customPartitionLocations, outputColumns),
+      hadoopConf = hadoopConf,
+      partitionColumns = partitionAttributes,
+      bucketSpec = bucketSpec,
+      statsTrackers = Seq(basicWriteJobStatsTracker(hadoopConf)),
+      options = options,
+    )
+  }
   override def withNewChildInternal(newChild: LogicalPlan): InsertIntoHiveTable = super.withNewChildInternal(newChild)
 }
